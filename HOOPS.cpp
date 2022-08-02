b790a87659c475ebf86764d6f2c3de19c269a137
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-08-02 23:23:26 +0530

    Create HOOPS.cpp

---
+++ HOOPS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T;
+	cin>>T;
+	while(T--){
+	    int n;
+	    cin>>n;
+	    cout<<(n+1)/2<<endl;
+	}
+	return 0;
+}
