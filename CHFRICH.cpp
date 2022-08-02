5abecd81dd957f8ab3296207af8e29e3bcc9c775
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-08-02 23:22:53 +0530

    Create CHFRICH.cpp

---
+++ CHFRICH.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+	    int a,b,x;
+	    cin>>a>>b>>x;
+	    cout<<(b-a)/x<<endl;
+	}
+	return 0;
+}
